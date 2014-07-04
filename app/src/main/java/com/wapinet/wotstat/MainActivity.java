4aac276be346f5e310e5bc9848f43087b7ffb5af
Gemorroj <wapinet@gmail.com>
2014-07-04 10:31:35 +0300

    добавлено отображение других рейтингов

--- app/src/main/java/com/wapinet/wotstat/MainActivity.java
+++ app/src/main/java/com/wapinet/wotstat/MainActivity.java
@@ -26,13 +26,16 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
     final String applicationId = "demo";
     protected Integer userId = null;
-    protected String apiUrl = "http://api.worldoftanks.ru/wot/account/list/?application_id=" + applicationId;
+    protected String apiListUrl = "http://api.worldoftanks.ru/wot/account/list/?application_id=" + applicationId;
+    protected String apiRatingsUrl = "http://api.worldoftanks.ru/wot/ratings/accounts/?application_id=" + applicationId;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
@@ -44,14 +47,16 @@ public class MainActivity extends Activity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -65,9 +70,11 @@ public class MainActivity extends Activity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment
+    {
 
-        public PlaceholderFragment() {
+        public PlaceholderFragment()
+        {
         }
 
         @Override
@@ -78,29 +85,220 @@ public class MainActivity extends Activity {
     }
 
 
-
-
-
-    public void buttonSearchClick(View v){
+    /**
+     * @param v Button view
+     */
+    public void buttonSearchClick(View v)
+    {
         try {
             String username = ((EditText) findViewById(R.id.editText)).getText().toString();
 
-            JSONObject json = (new HttpRequest()).execute(this.apiUrl + "&search=" + username).get();
-            handleRequest(json);
+            JSONObject list = (new HttpRequest()).execute(this.apiListUrl + "&search=" + username).get();
+            handleRequest(list);
 
-            this.userId = json.getJSONArray("data").getJSONObject(0).getInt("account_id");
+            this.userId = list.getJSONArray("data").getJSONObject(0).getInt("account_id");
+
+
+            JSONObject rating = (new HttpRequest()).execute(this.apiRatingsUrl + "&type=all&account_id=" + this.userId).get();
+            handleRequest(rating);
+
+
 
             TextView resultTextViewWN6 = (TextView) findViewById(R.id.resultTextViewWN6);
-            resultTextViewWN6.setText(json.toString());
+            resultTextViewWN6.setText(calculateWn6Rating(rating).toString());
+
+            TextView resultTextViewWN7 = (TextView) findViewById(R.id.resultTextViewWN7);
+            resultTextViewWN7.setText(calculateWn7Rating(rating).toString());
 
             TextView resultTextViewWN8 = (TextView) findViewById(R.id.resultTextViewWN8);
-            resultTextViewWN8.setText(this.userId.toString());
+            resultTextViewWN8.setText(calculateWn8Rating(rating).toString());
+
+            TextView resultTextViewPf = (TextView) findViewById(R.id.resultTextViewPf);
+            resultTextViewPf.setText(calculatePfRating(rating).toString());
+
+            TextView resultTextViewArmorSite = (TextView) findViewById(R.id.resultTextViewArmorSite);
+            resultTextViewArmorSite.setText(calculateArmorSiteRating(rating).toString());
+
         } catch (Exception e) {
             createErrorMessage(e.getMessage());
         }
     }
 
 
+    protected Integer calculateWn8Rating(JSONObject data)
+    {
+        /*
+        Формула WN8:
+        Шаг 1.
+
+        взвешенное соотношение = суммарный показатель на аккаунте / ожидаемый показатель
+        rDAMAGE = avgDmg / expDmg
+        rSPOT = avgSpot / expSpot
+        rFRAG = avgFrag / expFrag
+        rDEF = avgDef / expDef
+        rWIN = avgWinRate / expWinRate
+
+        Шаг 2.
+
+        Нормализованное значение =  max(нижняя граница, (взвешенное соотношение – константа ) / (1 – константа))
+        rWINc = max(0, (rWIN - 0.71) / (1- 0.71))
+        rDAMAGEc= max(0, (rDAMAGE-0.22) / (1-0.22))
+        Нормализованное значение = min(верхняя граница, max(нижняя граница, (взвешенное соотношение – константа ) / (1 – константа)))
+        rFRAGc = min(rDAMAGEc+0.2 , max(0, (rFRAG-0.12) / (1-0.12)))
+        rSPOTc = min (rDAMAGEc+0.1 ,  max(0, (rSPOT-0.38) / (1-0.38)))
+        rDEFc =min (rDAMAGEc+0.1 , max(0, (rDEF-0.10) / (1-0.10)))
+
+        Шаг3
+
+        WN8 = 980*rDAMAGEc + 210*rDAMAGEc*rFRAGc + 155*rFRAGc*rSPOTc + 75*rDEFc*rFRAGc + 145*MIN(1.8,rWINc)
+
+        Параметры, участвующие в расчете шага 1:
+        avgDmg - суммарный нанесенный урон игрока,
+        avgSpot - суммарное количество обнаруженных,
+        avgFrag - суммарное количество уничтоженных,
+        avgDef - суммарное количество очков защиты,
+        avgWinRate - суммарное количество побед,
+        expDmg - ожидаемый нанесенный урон,
+        expSpot - ожидаемое количество обнаруженных,
+        expFrag - ожидаемое количество уничтоженных,
+        expDef - ожидаемое количество очков защиты,
+        expWinRate - ожидаемое количество побед.
+         */
+
+        return new Integer(123);
+    }
+
+    protected Integer calculateWn7Rating(JSONObject data)
+    {
+        /*
+        Формула WN7:
+
+        (1240-1040/(min(TIER,6)^0.164))*FRAGS
+        + DAMAGE * 530/(184*exp(0.24*TIER)+130)
+        + SPOT * 125*min(TIER,3)/3
+        + min(DEF,2.2)*100
+        + ((185/(0.17+exp(((WINRATE)-35)*-0.134)))-500)*0.45
+        + (-1*(((5 - min(TIER,5))*125)/(1 + exp((TIER-(TOTAL/220^(3/TIER)))*1.5))))
+
+
+        Параметры, участвующие в расчете:
+        DAMAGE – средний урон за бой,
+        TIER – средний уровень танков игрока,
+        FRAGS – среднее количество фрагов за бой,
+        SPOT – среднее количество обнаруженных врагов,
+        DEF - среднее количество очков защиты базы за бой,
+        WINRATE – процент побед, умноженный на 100,
+        TOTAL – общее кол-во боёв.
+         */
+
+        return new Integer(123);
+    }
+
+    protected Integer calculateWn6Rating(JSONObject data)
+    {
+        /*
+        Формула WN6:
+
+        (1240-1040/(MIN(TIER,6))^0.164)*FRAGS
+        +DAMAGE*530/(184*exp(0.24*TIER)+130)
+        +SPOT*125
+        +MIN(DEF,2.2)*100
+        +((185/(0.17+exp((WINRATE-35)*-0.134)))-500)*0.45
+        +(6-MIN(TIER,6))*-60
+
+
+        Параметры, участвующие в расчете:
+        DAMAGE – средний урон за бой,
+        TIER – средний уровень танков игрока,
+        FRAGS – среднее количество фрагов за бой,
+        SPOT – среднее количество обнаруженных врагов,
+        DEF - среднее количество очков защиты базы за бой,
+        WINRATE – процент побед, умноженный на 100,
+        TOTAL – общее кол-во боёв.
+         */
+
+        return new Integer(123);
+    }
+
+
+    protected Integer calculateEffRating(JSONObject data)
+    {
+        /*
+        Формула:
+
+        DAMAGE * (10 / (TIER + 2)) * (0.23 + 2*TIER / 100) +
+        FRAGS * 250 +
+        SPOT * 150 +
+        log(CAP + 1,1.732) * 150 +
+        DEF * 150;
+
+        Параметры, участвующие в расчете:
+        DAMAGE – средний урон за бой,
+        TIER – средний уровень танков игрока,
+        FRAGS – среднее количество фрагов за бой,
+        SPOT – среднее количество обнаруженных врагов,
+        CAP – среднее количество очков захвата,
+        DEF - среднее количество очков защиты базы за бой.
+         */
+
+        return new Integer(123);
+    }
+
+
+    protected Integer calculatePfRating(JSONObject data)
+    {
+        /*
+        Формула:
+
+        DAMAGE * (10 / (TIER + 2)) * (0.23 + 2*TIER / 100) +
+        FRAGS * 250 +
+        SPOT * 150 +
+        log(CAP + 1,1.732) * 150 +
+        DEF * 150;
+
+        Параметры, участвующие в расчете:
+        DAMAGE – средний урон за бой,
+        TIER – средний уровень танков игрока,
+        FRAGS – среднее количество фрагов за бой,
+        SPOT – среднее количество обнаруженных врагов,
+        CAP – среднее количество очков захвата,
+        DEF - среднее количество очков защиты базы за бой.
+         */
+
+        return new Integer(123);
+    }
+
+
+    protected Integer calculateArmorSiteRating(JSONObject data)
+    {
+        /*
+        Формула:
+
+        (ln B)/10* Hp* 1.0+
+        (ln B)/10*D*WINRATE*2+
+        (ln B)/10*D*FRAGS*0.9+
+        (ln B)/10*D*SPOT*0.5+
+        (ln B)/10*D*CAP*0.5+
+        (ln B)/10*D*DEF*0.5
+
+        Параметры, участвующие в расчете:
+        D – средний урон за бой,
+        В- общее количество боёв,
+        Нр- средний опыт за бой,
+        WINRATE - процент побед игрока,
+        FRAGS – среднее количество фрагов за бой,
+        SPOT - среднее количество обнаруженных противников за бой,
+        CAP – среднее количество очков захвата за бой,
+        DEF - среднее количество очков защиты базы за бой.
+         */
+
+        return new Integer(123);
+    }
+
+
+    /**
+     * @param message Error message
+     */
     protected void createErrorMessage(String message)
     {
         (new AlertDialog.Builder(this)
@@ -113,27 +311,36 @@ public class MainActivity extends Activity {
     }
 
 
-    protected void handleRequest(JSONObject json) throws Exception
+    /**
+     * @param data JSON object
+     * @throws Exception
+     */
+    protected void handleRequest(JSONObject data) throws Exception
     {
-        if (null == json) {
+        if (null == data) {
             throw new Exception("Неизвестная ошибка.");
         }
 
-        if (!json.getString("status").equals("ok")) {
-            throw new Exception(json.getJSONObject("error").getString("message"));
+        if (!data.getString("status").equals("ok")) {
+            throw new Exception(data.getJSONObject("error").getString("message"));
         }
 
-        if (json.getInt("count") != 1) {
-            throw new Exception("Найдено " + String.valueOf(json.getInt("count")) + " пользователей.");
+        if (data.getInt("count") != 1) {
+            throw new Exception("Найдено " + String.valueOf(data.getInt("count")) + " пользователей.");
         }
     }
 
 
-    private class HttpRequest extends AsyncTask<String, Void, JSONObject> {
+    /**
+     * Async http request
+     */
+    private class HttpRequest extends AsyncTask<String, Void, JSONObject>
+    {
         private ProgressDialog spinner;
 
         @Override
-        protected void onPreExecute() {
+        protected void onPreExecute()
+        {
             spinner = new ProgressDialog(MainActivity.this);
             spinner.setMessage("Load...");
             spinner.setIndeterminate(false);
@@ -142,12 +349,14 @@ public class MainActivity extends Activity {
         }
 
         @Override
-        protected void onPostExecute(JSONObject json) {
+        protected void onPostExecute(JSONObject json)
+        {
             spinner.dismiss();
         }
 
         @Override
-        protected JSONObject doInBackground(String... links) {
+        protected JSONObject doInBackground(String... links)
+        {
             try {
                 URL url = new URL(links[0]);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
